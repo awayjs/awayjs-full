1af8d0186d8cc7f9ec68f24564a40de46910c5f3
rob-bateman <rob@infiniteturtles.co.uk>
2016-06-11 03:39:19 +0100

    update webpack update to be compatible with require() use case as well as global, access to lib packages and creation of corresponding d.ts files for publishable version

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,97 @@
-var path = require("path");
+var path = require('path');
+var fs = require('fs');
+var webpack = require('webpack');
+var CopyWebPackPlugin = require('copy-webpack-plugin');
+
+var subModules = fs.readdirSync(path.join(__dirname, "lib")).filter(function (file) {
+    return (file.slice(-3) == ".ts");
+});
+var entry = {};
+
+for (var i = 0; i < subModules.length; i++) {
+    var name = subModules[i].split('.')[0];
+    entry[String(name)] = [path.join(__dirname, "lib", subModules[i])];
+}
+
+entry['awayjs-full'] = ['./index'];
 
 module.exports = {
-    entry: ['./index.ts'],
+
+    entry: entry,
+    devtool: 'source-map',
     output: {
-        path: './dist',
-        filename: 'awayjs-full.js',
+        path: path.join(__dirname, "dist"),
+        filename: 'lib/[name].js',
         libraryTarget: 'umd',
         umdNamedDefine: true,
-        library: "awayjs-full"
+        library: "[name]"
     },
-    //turn on sourcemaps
-    devtool: 'source-map',
     resolve: {
+        alias: {
+            "awayjs-core": "awayjs-core/dist",
+			"awayjs-display": "awayjs-display/dist",
+			"awayjs-stagegl": "awayjs-stagegl/dist",
+			"awayjs-renderergl": "awayjs-renderergl/dist",
+			"awayjs-methodmaterials": "awayjs-methodmaterials/dist",
+			"awayjs-player": "awayjs-player/dist",
+			"awayjs-parsers": "awayjs-parsers/dist"
+        },
         // Add `.ts` and `.tsx` as a resolvable extension.
         extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
         fallback: [path.join(__dirname, 'node_modules')]
     },
     resolveLoader: {
+        root: path.join(__dirname, 'node_modules'),
         fallback: [path.join(__dirname, 'node_modules')]
     },
     module: {
         loaders: [
-            // all files with a `.ts` or `.tsx` extension will be handled by 'awesome-typescript-loader'
-            { test: /\.ts(x?)$/, loader: require.resolve('awesome-typescript-loader') },
+            // all files with a `.ts` or `.tsx` extension will be handled by `awesome-typescript-loader`
+            { test: /\.ts(x?)$/, loader: require.resolve('awesome-typescript-loader')},
 
+            // all files with a `.js` or `.jsx` extension will be handled by `source-map-loader`
             { test: /\.js(x?)$/, loader: require.resolve('source-map-loader') }
         ]
-    }
-}
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({name:'awayjs-full', filename:'index.js'}),
+        new CopyWebPackPlugin([
+            {
+                context: "./node_modules/awayjs-core/dist",
+                from: "**/*.d.ts",
+                to: './node_modules/awayjs-core'
+            },
+            {
+                context: "./node_modules/awayjs-display/dist",
+                from: "**/*.d.ts",
+                to: './node_modules/awayjs-display'
+            },
+            {
+                context: "./node_modules/awayjs-stagegl/dist",
+                from: "**/*.d.ts",
+                to: './node_modules/awayjs-stagegl'
+            },
+            {
+                context: "./node_modules/awayjs-renderergl/dist",
+                from: "**/*.d.ts",
+                to: './node_modules/awayjs-renderergl'
+            },
+            {
+                context: "./node_modules/awayjs-methodmaterials/dist",
+                from: "**/*.d.ts",
+                to: './node_modules/awayjs-methodmaterials'
+            },
+            {
+                context: "./node_modules/awayjs-player/dist",
+                from: "**/*.d.ts",
+                to: './node_modules/awayjs-player'
+            },
+            {
+                context: "./node_modules/awayjs-parsers/dist",
+                from: "**/*.d.ts",
+                to: './node_modules/awayjs-parsers'
+            }
+            
+        ])
+    ]
+};
