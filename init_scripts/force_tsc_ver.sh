ee5c57a390c06be9f5555ff702689dbe48fa403e
Alejandro Santander <palebluedot@gmail.com>
2016-12-02 19:20:28 -0300

    script to force install a spaceific tsc version on awayjs submodules

---
+++ init_scripts/force_tsc_ver.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+repos=( 
+  "./../core/"
+  "./../graphics/"
+  "./../materials/"
+  "./../parsers/"
+  "./../player/"
+  "./../renderer/"
+  "./../scene/"
+  "./../stage/"
+  "./../view/"
+)
+
+ver=$1
+
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+echo "Forcing typescript version: '$ver'"
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+for repo in "${repos[@]}"
+do
+  
+  echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>. $repo"
+  echo cd "${repo}"
+  echo npm install typescript@"$ver"
+  echo head -20 ./node_modules/typescript/package.json
+  
+  cd "${repo}"
+  npm install typescript@"$ver"
+  head -20 ./node_modules/typescript/package.json
+  
+done
+
+
+
+
+
