4fe2dbaefb4002a488a6b2f10b250921912a7804
rob-bateman <rob@infiniteturtles.co.uk>
2017-12-12 00:11:15 +0000

    refactor to better group / separate functionality across modules.

--- lib/view.ts
+++ lib/view.ts
@@ -1,16 +1,16 @@
 export * from "@awayjs/view";
 
-import {Loader, WaveAudioParser, AttributesBuffer} from "@awayjs/core";
+import {Loader, WaveAudioParser} from "@awayjs/core";
 
-import {Image2DParser, ImageCubeParser, TextureAtlasParser, Shape, Image2D, ImageCube, BitmapImage2D, ExternalImage2D, BitmapImageCube, SpecularImage2D, Sampler2D, SamplerCube, LineElements, TriangleElements, Single2DTexture, SingleCubeTexture, BasicMaterial} from "@awayjs/graphics";
+import {GL_LineElements, Shape, LineElements, TriangleElements, GL_ShapeRenderable, GL_TriangleElements} from "@awayjs/graphics";
 
-import {Stage, GL_AttributesBuffer, GL_RenderImage2D, GL_RenderImageCube, GL_BitmapImage2D, GL_ExternalImage2D, GL_BitmapImageCube, GL_Sampler2D, GL_SamplerCube, RenderablePool, ShaderBase, GL_ShapeRenderable, GL_TriangleElements, GL_Single2DTexture, GL_SingleCubeTexture, GL_BasicMaterial} from "@awayjs/stage";
+import {Stage, Image2D, ImageCube, BitmapImage2D, ExternalImage2D, BitmapImageCube, SpecularImage2D, Image2DParser, ImageCubeParser, TextureAtlasParser, AttributesBuffer, GL_AttributesBuffer, GL_RenderImage2D, GL_RenderImageCube, GL_BitmapImage2D, GL_ExternalImage2D, GL_BitmapImageCube} from "@awayjs/stage";
 
-import {Skybox, DisplayObjectContainer, Scene, Billboard, LineSegment, Camera, Sprite, MorphSprite, MovieClip, TextField} from "@awayjs/scene";
+import {GL_LineSegmentRenderable, GL_SkyboxRenderable, GL_SkyboxMaterial, GL_BillboardRenderable, Skybox, DisplayObjectContainer, Scene, Billboard, LineSegment, Camera, Sprite, MorphSprite, MovieClip, TextField} from "@awayjs/scene";
 
-import {GL_SkyboxMaterial, GL_BillboardRenderable, GL_LineSegmentRenderable, GL_SkyboxRenderable, GL_LineElements, DefaultMaterialGroup} from "@awayjs/renderer";
+import {RenderStatePool, ShaderBase} from "@awayjs/renderer";
 
-import {MethodMaterial, GL_MethodMaterial} from "@awayjs/materials";
+import {MethodMaterial, GL_MethodMaterial, BasicMaterial, GL_BasicMaterial, ImageTexture2D, ImageTextureCube, GL_ImageTexture2D, GL_ImageTexture} from "@awayjs/materials";
 
 import {PartitionBase, CameraNode, EntityNode, SkyboxNode, ViewImage2D} from "@awayjs/view";
 
@@ -23,13 +23,10 @@ Loader.enableParser(TextureAtlasParser);
 //register core attributes
 Stage.registerAbstraction(GL_AttributesBuffer, AttributesBuffer);
 
-//register scene entities
-DefaultMaterialGroup.registerAbstraction(GL_SkyboxMaterial, Skybox);
-
-RenderablePool.registerAbstraction(GL_BillboardRenderable, Billboard);
-RenderablePool.registerAbstraction(GL_LineSegmentRenderable, LineSegment);
-RenderablePool.registerAbstraction(GL_ShapeRenderable, Shape);
-RenderablePool.registerAbstraction(GL_SkyboxRenderable, Skybox);
+RenderStatePool.registerAbstraction(GL_BillboardRenderable, Billboard);
+RenderStatePool.registerAbstraction(GL_LineSegmentRenderable, LineSegment);
+RenderStatePool.registerAbstraction(GL_ShapeRenderable, Shape);
+RenderStatePool.registerAbstraction(GL_SkyboxRenderable, Skybox);
 
 //register graphics images
 Stage.registerAbstraction(GL_RenderImage2D, Image2D);
@@ -39,22 +36,10 @@ Stage.registerAbstraction(GL_ExternalImage2D, ExternalImage2D);
 Stage.registerAbstraction(GL_BitmapImageCube, BitmapImageCube);
 Stage.registerAbstraction(GL_BitmapImage2D, SpecularImage2D);
 Stage.registerAbstraction(GL_BitmapImage2D, ViewImage2D);
-Stage.registerAbstraction(GL_Sampler2D, Sampler2D);
-Stage.registerAbstraction(GL_SamplerCube, SamplerCube);
 
 //register graphics elements
 Stage.registerAbstraction(GL_LineElements, LineElements);
 Stage.registerAbstraction(GL_TriangleElements, TriangleElements);
-
-//register graphics textures
-ShaderBase.registerAbstraction(GL_Single2DTexture, Single2DTexture);
-ShaderBase.registerAbstraction(GL_SingleCubeTexture, SingleCubeTexture);
-
-//register graphics materials
-DefaultMaterialGroup.registerAbstraction(GL_BasicMaterial, BasicMaterial);
-
-//regiater material materials
-DefaultMaterialGroup.registerAbstraction(GL_MethodMaterial, MethodMaterial);
 
 //register view nodes
 PartitionBase.registerAbstraction(CameraNode, Camera);
