f2bd46cb7ce6c478f95520a618dc4269e6f57ed8
rob-bateman <rob@infiniteturtles.co.uk>
2017-12-14 17:15:36 +0000

    refactor to abstraction classes, move regsitration out of awayjs-full, combine abstraction classes with class files

--- lib/view.ts
+++ lib/view.ts
@@ -2,44 +2,19 @@ export * from "@awayjs/view";
 
 import {Loader, WaveAudioParser} from "@awayjs/core";
 
-import {GL_LineElements, Shape, LineElements, TriangleElements, GL_ShapeRenderable, GL_TriangleElements} from "@awayjs/graphics";
+import {Shape} from "@awayjs/graphics";
 
-import {Stage, Image2D, ImageCube, BitmapImage2D, ExternalImage2D, BitmapImageCube, SpecularImage2D, Image2DParser, ImageCubeParser, TextureAtlasParser, AttributesBuffer, GL_AttributesBuffer, GL_RenderImage2D, GL_RenderImageCube, GL_BitmapImage2D, GL_ExternalImage2D, GL_BitmapImageCube} from "@awayjs/stage";
+import {Stage, Image2DParser, ImageCubeParser, TextureAtlasParser} from "@awayjs/stage";
 
-import {GL_LineSegmentRenderable, GL_SkyboxRenderable, GL_SkyboxMaterial, GL_BillboardRenderable, Skybox, DisplayObjectContainer, Scene, Billboard, LineSegment, Camera, Sprite, MorphSprite, MovieClip, TextField} from "@awayjs/scene";
+import {Skybox, DisplayObjectContainer, Scene, Billboard, LineSegment, Camera, Sprite, MorphSprite, MovieClip, TextField} from "@awayjs/scene";
 
-import {RenderStatePool, ShaderBase} from "@awayjs/renderer";
-
-import {MethodMaterial, GL_MethodMaterial, BasicMaterial, GL_BasicMaterial, ImageTexture2D, ImageTextureCube, GL_ImageTexture2D, GL_ImageTexture} from "@awayjs/materials";
-
-import {PartitionBase, CameraNode, EntityNode, SkyboxNode, ViewImage2D} from "@awayjs/view";
+import {PartitionBase, CameraNode, EntityNode, SkyboxNode} from "@awayjs/view";
 
 //enable parsers
 Loader.enableParser(WaveAudioParser);
 Loader.enableParser(Image2DParser);
 Loader.enableParser(ImageCubeParser);
 Loader.enableParser(TextureAtlasParser);
-
-//register core attributes
-Stage.registerAbstraction(GL_AttributesBuffer, AttributesBuffer);
-
-RenderStatePool.registerAbstraction(GL_BillboardRenderable, Billboard);
-RenderStatePool.registerAbstraction(GL_LineSegmentRenderable, LineSegment);
-RenderStatePool.registerAbstraction(GL_ShapeRenderable, Shape);
-RenderStatePool.registerAbstraction(GL_SkyboxRenderable, Skybox);
-
-//register graphics images
-Stage.registerAbstraction(GL_RenderImage2D, Image2D);
-Stage.registerAbstraction(GL_RenderImageCube, ImageCube);
-Stage.registerAbstraction(GL_BitmapImage2D, BitmapImage2D);
-Stage.registerAbstraction(GL_ExternalImage2D, ExternalImage2D);
-Stage.registerAbstraction(GL_BitmapImageCube, BitmapImageCube);
-Stage.registerAbstraction(GL_BitmapImage2D, SpecularImage2D);
-Stage.registerAbstraction(GL_BitmapImage2D, ViewImage2D);
-
-//register graphics elements
-Stage.registerAbstraction(GL_LineElements, LineElements);
-Stage.registerAbstraction(GL_TriangleElements, TriangleElements);
 
 //register view nodes
 PartitionBase.registerAbstraction(CameraNode, Camera);
