f683c51ccb3539b4d61dece5df90dda11eab063f
Rob Bateman <rob@infiniteturtles.co.uk>
2018-04-11 00:26:08 +0100

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,21 @@
-var includePaths = require('rollup-plugin-includepaths');
+var commonjs = require('rollup-plugin-commonjs');
+var nodeResolve = require('rollup-plugin-node-resolve');
 
 module.exports = {
-	entry: './dist/index.js',
-	sourceMap: true,
-	format: 'umd',
-	moduleName: 'AwayjsFull',
-	targets: [
-		{ dest: './bundle/awayjs-full.umd.js'}
-	],
+	input: './dist/index.js',
+	output: {
+		name: 'AwayjsFull',
+		sourcemap: true,
+		format: 'umd',
+		file: './bundle/awayjs-full.umd.js'
+	},
 	plugins: [
-		includePaths({
-			include : {
-				"tslib": "./node_modules/tslib/tslib.es6.js",
-				"@awayjs/core": "./node_modules/@awayjs/core/dist/index.js",
-				"@awayjs/graphics": "./node_modules/@awayjs/graphics/dist/index.js",
-				"@awayjs/scene": "./node_modules/@awayjs/scene/dist/index.js",
-				"@awayjs/stage": "./node_modules/@awayjs/stage/dist/index.js",
-				"@awayjs/renderer": "./node_modules/@awayjs/renderer/dist/index.js",
-				"@awayjs/view": "./node_modules/@awayjs/view/dist/index.js",
-				"@awayjs/materials": "./node_modules/@awayjs/materials/dist/index.js",
-				"@awayjs/player": "./node_modules/@awayjs/player/dist/index.js",
-				"@awayjs/parsers": "./node_modules/@awayjs/parsers/dist/index.js"
-			}
+		nodeResolve({
+			jsnext: true,
+			main: true,
+			module: true
+		}),
+		commonjs({
+			include: /node_modules/
 		}) ]
 };
